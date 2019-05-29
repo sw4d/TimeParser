a06f7aff4dc1e73db0ebf4226b1469a36aaa32a2
Unknown <stephen.4d@gmail.com>
2019-05-28 23:16:59 -0500

    Swapping hash variables to classes
    still need to add some decent descriptions

--- affiniPaySample.rb
+++ affiniPaySample.rb
@@ -14,8 +14,8 @@
 
 def add_minutes(time, additional_minutes)
   basic_type_check(time, additional_minutes)
-  new_time = append_times(hashify_time(time), additional_minutes)
-  "#{new_time[:hour]}:#{format('%02d', new_time[:minute])} #{new_time[:period]}"
+  new_time = AppendTimeToSet.new(ParsedTime.new(time), additional_minutes)
+  "#{new_time.hour}:#{format('%02d', new_time.minute)} #{new_time.period}"
 end
 
 private
@@ -31,48 +31,36 @@ def basic_type_check(time, minutes)
   raise 'Unexpected type passed for additional_minutes variable - should be integer' unless minutes.is_a? Integer
 end
 
-############################################################
-# when initially stepping into this method the time variable
-# will look  something like: 'HH:MM {AM:PM}'.
-#
-# This method breaks that string into a hash so it's
-# easier to work with down the road.
-############################################################
+class AppendTimeToSet
+  def initialize(base_time, additional_mins)
+    time_to_add = TimeToAdd.new(base_time.minute, additional_mins)
+    hour_to_add = base_time.hour
+    period = base_time.period
 
-def hashify_time(time)
-  split_time = time.split('_')[0].split(':')
-
-  {
-    minute: split_time[1].to_i,
-    hour: split_time[0].to_i,
-    period: time.split(' ')[1]
-  }
-end
-
-############################################################
-# To combine times we first parse our additional_minutes
-# into a format similar to the base_time hash.
-
-# Variable, 'time_to_add' is a hash with the number of hours
-# and minutes we need to add to our base_time. We append
-# every new hour and flip the time period as needed.
-############################################################
-
-def append_times(base_time, additional_mins)
-  time_to_add = parse_minutes_to_time_hash(base_time[:minute], additional_mins)
-  (1..time_to_add[:hours]).each do |_|
-    base_time[:hour] += 1
-    if base_time[:hour] >= 12
-      base_time[:hour] = 1 if base_time[:hour] > 12
-      base_time[:period] = flip_period[base_time[:period].to_sym]
+    (1..time_to_add.hours).each do |_|
+      hour_to_add += 1
+      if hour_to_add >= 12
+        hour_to_add = 1 if hour_to_add > 12
+        period = flip_period[period.to_sym]
+      end
     end
+
+    @hour = hour_to_add
+    @minute = time_to_add.minutes
+    @period = period
   end
 
-  {
-    hour: base_time[:hour],
-    minute: time_to_add[:minutes],
-    period: base_time[:period]
-  }
+  def hour
+    @hour
+  end
+
+  def minute
+    @minute
+  end
+
+  def period
+    @period
+  end
 end
 
 ############################################################
@@ -84,18 +72,42 @@ def flip_period
   { AM: 'PM', PM: 'AM' }
 end
 
-############################################################
-# This method takes an integer of minutes and returns a hash
-# of the total number of hours that can be parsed from them.
-# The remaining minutes are returned as well.
-############################################################
+class TimeToAdd
+  def initialize(base_minutes, additional_minutes)
+    if additional_minutes.zero?
+      @add_hours = 0
+    else
+      @add_hours = (0..additional_minutes).step(60).size - 1 
+      @add_hours += 1 if base_minutes + (additional_minutes % 60) >= 60
+    end
 
-def parse_minutes_to_time_hash(base_minutes, additional_minutes)
-  return { hours: 0, minutes: base_minutes } if additional_minutes.zero?
+      @additional_minutes = (base_minutes + additional_minutes) % 60
+  end
 
-  add_hours = (0..additional_minutes).step(60).size - 1
-  add_hours += 1 if base_minutes + (additional_minutes % 60) >= 60
-  additional_minutes = (base_minutes + additional_minutes) % 60
+  def hours
+    @add_hours
+  end
 
-  { hours: add_hours, minutes: additional_minutes }
+  def minutes
+    @additional_minutes
+  end
+end
+
+class ParsedTime
+  def initialize(time)
+    @time_period = time.split(' ')[1]
+    @time_split_hour_minute = time.split('_')[0].split(':')
+  end
+
+  def period
+    @time_period
+  end
+
+  def minute
+    @time_split_hour_minute[1].to_i
+  end
+
+  def hour
+    @time_split_hour_minute[0].to_i
+  end
 end
